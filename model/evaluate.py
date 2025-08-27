52dc2da07878b2ef1f115d7fda35b32aa5f0a068
archit nandan <149472441+sauravkrpal@users.noreply.github.com>
2025-08-27 08:58:54 +0530

    Update evaluate.py

--- model/evaluate.py
+++ model/evaluate.py
@@ -10,7 +10,7 @@ X = data.drop("Churn", axis=1)   # assuming your target column is "Churn"
 y = data["Churn"]
 
 # Load the trained model
-model = joblib.load("model/customer_segmentation.pkl")
+model = joblib.load("model/churn_model.pkl")
 
 # Make predictions
 y_pred = model.predict(X)
