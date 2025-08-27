d4a487aa413016452c4972444df952c7eeae3470
archit nandan <149472441+sauravkrpal@users.noreply.github.com>
2025-08-27 08:56:11 +0530

    Update evaluate.py

--- model/evaluate.py
+++ model/evaluate.py
@@ -10,7 +10,7 @@ X = data.drop("Churn", axis=1)   # assuming your target column is "Churn"
 y = data["Churn"]
 
 # Load the trained model
-model = joblib.load("model/churn_model.pkl")
+model = joblib.load("model/cc_model.pkl")
 
 # Make predictions
 y_pred = model.predict(X)
